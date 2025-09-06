05e1c4ca883dfadc4e0257f481ee2c946b52ff84
Raúl Armas <raularmasbx@gmail.com>
2025-09-06 00:46:56 -0500

    updates

--- src/main/java/com/maestria/springmvcstock/infrastructure/clients/QueryServiceClient.java
+++ src/main/java/com/maestria/springmvcstock/infrastructure/clients/QueryServiceClient.java
@@ -8,9 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import com.maestria.springmvcstock.model.Customer;
-import com.maestria.springmvcstock.model.Product;
-import com.maestria.springmvcstock.model.Supplier;
+ 
 
 @Service
 public class QueryServiceClient {
@@ -20,23 +18,23 @@ public class QueryServiceClient {
     private final String SERVICE_URL = "http://StockQueryService/api/v1";
     // private final String API_GATEWAY_BASE_URL = "http://localhost:8080/api/v1"; //http://localhost:8080/api/v1
 
-    public Customer getCustomerById(Long customerId) {
-        return restTemplate.getForObject(SERVICE_URL + "/customers/" + customerId, Customer.class);
-    }
+    // public Customer getCustomerById(Long customerId) {
+    //     return restTemplate.getForObject(SERVICE_URL + "/customers/" + customerId, Customer.class);
+    // }
 
-    public Product getProductById(Long productId) {
-        return restTemplate.getForObject(SERVICE_URL + "/products/" + productId, Product.class);
-    }
+    // public Product getProductById(Long productId) {
+    //     return restTemplate.getForObject(SERVICE_URL + "/products/" + productId, Product.class);
+    // }
 
-    public Supplier getSupplierById(Long supplierId) {
-        return restTemplate.getForObject(SERVICE_URL + "/suppliers/" + supplierId, Supplier.class);
-    }
+    // public Supplier getSupplierById(Long supplierId) {
+    //     return restTemplate.getForObject(SERVICE_URL + "/suppliers/" + supplierId, Supplier.class);
+    // }
 
-    public List<Customer> getCustomers() {
-        System.out.println("pbt");
-        ResponseEntity<List<Customer>> response =
-            restTemplate.exchange(SERVICE_URL + "/customers", HttpMethod.GET, null,
-            new ParameterizedTypeReference<List<Customer>>() {});
-        return response.getBody();
-    }
+    // public List<Customer> getCustomers() {
+    //     System.out.println("pbt");
+    //     ResponseEntity<List<Customer>> response =
+    //         restTemplate.exchange(SERVICE_URL + "/customers", HttpMethod.GET, null,
+    //         new ParameterizedTypeReference<List<Customer>>() {});
+    //     return response.getBody();
+    // }
 }
